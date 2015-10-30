c5a930aadaaae8d4c9306d01ee16ca00d6f1e753
jiteshjhawar <jiteshjhawar@gmail.com>
2015-10-30 13:43:07 +0530

    modified to visualize using SDL

--- src/main.cpp
+++ src/main.cpp
@@ -27,53 +27,49 @@ int main (int argc, char *argv[]){
 	Swarm swarm(particles, systemSize);
 	swarm.allocate();
 	swarm.launchRandInit((unsigned long) t);
-	for (float eta = maxeta; eta <= maxeta; eta = eta + 1.0){
+	for (float eta = 0.0; eta <= maxeta; eta = eta + 1.0){
 		store.orientationParam = 0.0;
 		for (int rep = 0; rep < realisations; rep++){	
 			swarm.init(eta);
 			swarm.cudaCopy();
-			/*Screen screen;
+			Screen screen;
 			if (screen.init() == false) {
 				cout << "error initialising SDL." << endl;
-			}*/
+			}
 			c = 0;
 			for (int i = 0; i < iterations; i++){
-				//screen.clear();
+				screen.clear();
 				swarm.update();
-				//swarm.cudaBackCopy();
+				swarm.cudaBackCopy();
 				//store.msd[i] += swarm.calcMSD(); //store msd for each time step in an array also sum each value for many
 				/*theta = swarm.returnRandTheta();
 				store.print(theta, i);*/
 				const Particle * const pParticles = swarm.returnParticles();
-				if (i >= 1000){
-					swarm.cudaBackCopy();
-					for (int p = 0; p < particles; p++){
-						Particle particle = pParticles[p];
+				swarm.cudaBackCopy();
+				for (int p = 0; p < particles; p++){
+					Particle particle = pParticles[p];
 
-						float x = particle.coord.x/* * Screen::SCREEN_WIDTH / systemSize*/;
-						float y = particle.coord.y/* * Screen::SCREEN_HEIGHT / systemSize*/;
-						store.printCoord(x,y);
-						//screen.setPixel(x, y, 125, 255, 125);
+					float x = particle.coord.x * Screen::SCREEN_WIDTH / systemSize;
+					float y = particle.coord.y * Screen::SCREEN_HEIGHT / systemSize;
+					//store.printCoord(x,y);
+					screen.setPixel(x, y, 125, 255, 125);
 					}
-					//screen.update();
-				
-				 			//...many realisations at each time step
-					
-					store.orientationParam = swarm.calcOrderparam();
-					store.print(eta);
-					store.endl();
+				screen.update();	
+				if (i >= 1000){
+					//swarm.cudaBackCopy();
+					store.orientationParam += swarm.calcOrderparam();
 				}
 				c++;
 				/*if (screen.processEvents() == false || c == iterations){
 					break;
 				}*/
 			}
-			//screen.close();
+			screen.close();
 			
 		}
-		//store.orientationParam = store.orientationParam / realisations / last;
-		//store.print(eta);
-		//store.endl();
+		store.orientationParam = store.orientationParam / realisations / last;
+		store.print(eta);
+		store.endl();
 		/*for (int i = 0; i < iterations; i++){
 			store.msd[i] = store.msd[i] / realisations / last / particles; //calculate average msd for each time step
 			store.print(eta, i);
